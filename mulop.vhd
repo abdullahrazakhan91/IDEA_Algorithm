e5304ea2a681d8df6433ab1b77494a51e03667e3
ARK91 <abdullahrazakhan91@gmail.com>
2017-11-07 20:40:09 +0100

    Update mulop.vhd

--- mulop.vhd
+++ mulop.vhd
@@ -1,22 +1,4 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    20:21:27 10/28/2014 
--- Design Name: 
--- Module Name:    mulop - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.std_logic_arith.all;
